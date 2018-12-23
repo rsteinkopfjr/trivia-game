aa0cf7396757703df8c6d655efb7c0904a9e63f3
rsteinkopfjr <rob.steinkopf@gmail.com>
2018-12-22 22:19:37 -0500

    Add final content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,21 @@ $(document).ready(function () {
 
     var triviaGame = {
         questions: [
-            {q: "What is 1+2?", al: ["1", "2", "3", "4"], ca: 2},
-            {q: "What is 2+2?", al: ["1", "2", "3", "4"], ca: 3},
-            {q: "What is 3-2?", al: ["1", "2", "3", "4"], ca: 0},
-            {q: "What is 1+1?", al: ["1", "2", "3", "4"], ca: 1},
+            {q: "From the movie Bambi, what type of animal is the character 'Flower'?", al: ["Rabbit", "Deer", "Skunk", "Hummingbird"], ca: 2},
+            {q: "What movie is the song 'Baby Mine' from?", al: ["Cinderella", "Tarzan", "Mulan", "Dumbo"], ca: 3},
+            {q: "Who is the villian in Aladdin?", al: ["Jafar", "Gaston", "Scar", "Ursula"], ca: 0},
+            {q: '"Hey look, is that Smith? That’s him alright, the old sea dog." Is the first line to which movie?', al: ["Robin Hood", "Pocahontas", "Sleeping Beauty", "The Little Mermaid"], ca: 1},
+            {q: 'What is the setting of "Lilo and Stitch"?', al: ["Florida", "Hawaii", "California", "Thailand"], ca: 1},
+            {q: "What is Sleeping Beauty's real name?", al: ["Megara", "Esmeralda", "Marian", "Aurora"], ca: 3},
+            {q: 'Which object is significant in the movie Dumbo?', al: ["Feather", "Balloon", "Pixie Dust", "Ball"], ca: 0},
+            {q: 'Which of the following is a Disney villian?', al: ["Prince Phillip", "Flynn Rider", "Prince John", "Prince Eric"], ca: 2},
+            {q: "In the movie Aladdin, what is the name of Princess Jasmine's pet?", al: ["Rajah", "Dinah", "Nana", "Meeko"], ca: 0},
+            {q: 'Which Disney movie has no human characters?', al: ["The Little Mermaid", "Robin Hood", "The Jungle Book", "Pinocchio"], ca: 1},
+            {q: 'In "The Rescuers", what is the name of one of the mice?', al: ["Terk", "Dale", "Gus Gus", "Bernard"], ca: 3},
+            {q: '"Puppy befriends but later hunts a wild vulpine." Describes which movie?', al: ["101 Dalmations", "The Princess and the Frog", "The Fox and the Hound", "The Rescuers"], ca: 2},
+            {q: 'Which Disney Princess spent her whole life in a tower?', al: ["Tiana", "Snow White", "Rapunzel", "Merida"], ca: 2},
+            {q: '"An epic of miniature proportions" was the tagline for which Disney movie?', al: ["A Bug's Life", "The Sword and the Stone", "Chicken Little", "Monster's Inc."], ca: 0},
+            {q: '"Oh, I just love happy endings! Yes, I do too. Oh, Blue? Pink! Blue!" is final line to which movie?', al: ["Pocahontas", "Cinderella", "Alice in Wonderland", "Sleeping Beauty"], ca: 3},
         ],
         messages: {
             correct: "Yes, that's correct!",
@@ -17,7 +28,6 @@ $(document).ready(function () {
             endTime: "Sorry, you are out of time!",
             finished: "Let's see how you did!"
         },
-        gifArray: [],
         currentQuestion: 0,
         correctAnswer: 0,
         incorrectAnswer: 0,
@@ -32,8 +42,8 @@ $(document).ready(function () {
 
     function newGame(){
         $("#final-message").empty();
-        $("#correct-answer").empty();
-        $("#incorrect-answer").empty();
+        $("#correct-answers").empty();
+        $("#incorrect-answers").empty();
         $("#unanswered").empty();
         triviaGame.currentQuestion = 0;
         triviaGame.correctAnswer = 0;
@@ -54,7 +64,7 @@ $(document).ready(function () {
             var choices = $("<button>");
             choices.text(triviaGame.questions[triviaGame.currentQuestion].al[i]);
             choices.attr({'data-index': i});
-            choices.addClass("this-choice");
+            choices.addClass("this-choice btn btn-light btn-lg");
             $(".trivia-answers").append(choices);
         }
         countdown();
@@ -89,7 +99,6 @@ $(document).ready(function () {
 
         var rightAnswerText = triviaGame.questions[triviaGame.currentQuestion].al[triviaGame.questions[triviaGame.currentQuestion].ca]
         var rightAnswerIndex = triviaGame.questions[triviaGame.currentQuestion].ca;
-        $("#gif").html("<img src='assets/images/" + triviaGame.gifArray[triviaGame.currentQuestion] + ".gif' width='400px'>");
 
         if((userSelect == rightAnswerIndex) && (answered == true)){
             triviaGame.correctAnswer++;
@@ -108,11 +117,11 @@ $(document).ready(function () {
         }
 
         if(triviaGame.currentQuestion == (triviaGame.questions.length-1)){
-            setTimeout(scoreboard, 5000)
+            setTimeout(scoreboard, 3000)
         }
         else{
             triviaGame.currentQuestion++;
-            setTimeout(newQuestion, 5000);
+            setTimeout(newQuestion, 3000);
         }
     }
 
